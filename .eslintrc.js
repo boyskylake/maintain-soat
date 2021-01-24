b328d325fc8868e03817792882ea061ab67a2032
boyskylake <auttapong.ni58@snru.ac.th>
2021-01-24 23:21:47 +0700

    commit by boy edit eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,25 +6,36 @@ module.exports = {
     env: {
         browser: true,
         node: true,
-        es6: true
+        es6: true,
     },
     globals: {
-        React: true
+        React: true,
     },
     parserOptions: {
         ecmaFeatures: {
-            jsx: true
-        }
+            jsx: true,
+        },
     },
     // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
     extends: ["eslint:recommended", "plugin:react/recommended"],
     // required to lint *.vue files
-    plugins: ["html", "react"],
+    plugins: ["html", "react", "react-hooks"],
     // add your custom rules here
     rules: {
         // allow paren-less arrow functions
         "arrow-parens": 0,
         // allow async-await
-        "generator-star-spacing": 0
-    }
+        "generator-star-spacing": 0,
+
+        // allow console during development
+        "no-console": process.env.NODE_ENV === "production" ? 2 : 0,
+        // allow debugger during development
+        "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0,
+
+        "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+        "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+
+        "no-empty": ["error", { allowEmptyCatch: true }],
+        "react/no-unescaped-entities": 0,
+    },
 };
