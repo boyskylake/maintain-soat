fb33b12743ec708d6a7775e6f94520e3cfbd447c
NUTTHAMON <78066321+natthamon0709@users.noreply.github.com>
2021-02-24 15:22:26 +0700

    nut

--- resources/js/src/Officer/layouts/Sidebar.js
+++ resources/js/src/Officer/layouts/Sidebar.js
@@ -119,6 +119,7 @@ const Sidebar = () => {
                             />
                             );
                         })}
+
                 </ul>
             </section>
             {/* /.sidebar */}
