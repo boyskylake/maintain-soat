7fa9a0d458511d7ab365c0413ca4ed6773c8b4a1
NUTTHAMON <78066321+natthamon0709@users.noreply.github.com>
2021-03-01 15:06:26 +0700

    nut

--- app/Officer.php
+++ app/Officer.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Officer extends Model
 {
-    protected $connection = 'mysql';
-    protected $table = 'officers';
+    // protected $connection = 'mysql';
+    // protected $table = 'officers';
 }
