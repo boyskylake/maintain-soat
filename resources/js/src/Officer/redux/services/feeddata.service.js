cadb83d23146706b67b4c77e8868b0b56b944901
kitti28575 <78080833+kitti28575@users.noreply.github.com>
2021-03-09 11:18:33 +0700

    bew add taiwind

--- resources/js/src/Officer/redux/services/feeddata.service.js
+++ resources/js/src/Officer/redux/services/feeddata.service.js
@@ -1,28 +1,31 @@
-import Services from "./services";
+import Services from './services';
 
 const service = new Services();
 
 function feedDataGet(url) {
     const requestOptions = {
-        method: "Get",
+        method: 'Get',
         //  body:   {}
     };
 
-    return service.API(url, requestOptions).then((res) => {
-        return res;
-    });
+    return service.API(url, requestOptions)
+        .then(res => {
+            return res;
+        });
 }
+
 function feedDataPost(url, body = {}) {
-    console.log(body)
     const requestOptions = {
-        method: "POST",
-        data: JSON.stringify(body),
+        method: "Post",
+        body:body,
     };
 
-    return service.API(url, requestOptions).then((res) => {
-        return res;
-    });
+    return service.API(url, requestOptions)
+        .then(res => {
+            return res;
+        });
 }
+
 
 export const feedDataService = {
     feedDataGet,
